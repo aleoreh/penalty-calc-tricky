332e0d33c5eabd4452873d201b7d47d625efbb36
Aleksey Orekhov <aleoreh@gmail.com>
2024-06-27 22:34:00 +0300

    Избавляет от зависимостей сценарий
    getCalculatorConfig

--- src/features/calculator/application/getCalculatorConfig.ts
+++ src/features/calculator/application/getCalculatorConfig.ts
@@ -1,13 +1,14 @@
+import { TheStateConfigRepo } from "../domain"
 import calculatorConfigShed from "../domain/calculator-config"
-import { CalculatorConfig } from "../domain/types"
-import { theStateConfigStaticRepo as theStateConfigRepo } from "../infrastructure/calculatorConfigStaticRepo"
 
 export { type CalculatorConfig } from "../domain/types"
 
-export async function getCalculatorConfig(
-    date: Date
-): Promise<CalculatorConfig> {
-    const theStateConfig = await theStateConfigRepo.getTheStateConfig()
+export function createGetCalculatorConfigUseCase(
+    theStateConfigRepo: TheStateConfigRepo
+) {
+    return async (date: Date) => {
+        const theStateConfig = await theStateConfigRepo.getTheStateConfig()
 
-    return calculatorConfigShed.fromTheStateConfig(date, theStateConfig)
+        return calculatorConfigShed.fromTheStateConfig(date, theStateConfig)
+    }
 }

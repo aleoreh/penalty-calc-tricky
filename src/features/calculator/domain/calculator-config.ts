2ad5b30eedbe7d41a269d7fb29e1177d2aca6038
Aleksey Orekhov <aleoreh@gmail.com>
2024-06-27 21:53:26 +0300

    Добавляет заглушку доменного преобразования
    - TheStateConfig -> CalculatorConfig

---
+++ src/features/calculator/domain/calculator-config.ts
@@ -0,0 +1,12 @@
+import { CalculatorConfig, TheStateConfig } from "./types"
+
+function fromTheStateConfig(theStateConfig: TheStateConfig): CalculatorConfig {
+    theStateConfig
+    throw new Error("not implemented")
+}
+
+export const calculatorConfigShed = {
+    fromTheStateConfig,
+}
+
+export default calculatorConfigShed

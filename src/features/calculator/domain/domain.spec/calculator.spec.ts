e3cddd4850d0978db4cfd64c21e51b1185902791
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-02 16:56:24 +0300

    Создаёт простой тест

---
+++ src/features/calculator/domain/domain.spec/calculator.spec.ts
@@ -0,0 +1,8 @@
+import { it } from "@fast-check/vitest"
+import { describe, expect } from "vitest"
+
+describe("Калькулятор", () => {
+    it("Проходит пустой тест", () => {
+        expect(true).toBe(true)
+    })
+})

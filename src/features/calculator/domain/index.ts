adb99778efedbeff6838296dc72703b1fb6f7886
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-03 20:52:17 +0300

    Удаляет неиспользуемый код

--- src/features/calculator/domain/index.ts
+++ src/features/calculator/domain/index.ts
@@ -3,5 +3,3 @@ import { TheStateConfig } from "./types"
 export type TheStateConfigRepo = {
     getTheStateConfig: () => Promise<TheStateConfig>
 }
-
-export { type Calculator } from "./calculator"

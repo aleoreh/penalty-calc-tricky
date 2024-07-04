c5d5a0cf346ed1154e61f4c4173c1d48bb4779b9
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-04 22:41:29 +0300

    WIP: тесты калькулятора

--- src/features/calculator/domain/domain.spec/calculator.spec.ts
+++ src/features/calculator/domain/domain.spec/calculator.spec.ts
@@ -1,12 +1,14 @@
-import { it } from "@fast-check/vitest"
-import { describe, expect } from "vitest"
-import calculatorShed from "../calculator"
-import calculatorConfigShed from "../calculator-config"
 import * as data from "@/data"
+import { it } from "@fast-check/vitest"
 import { pipe } from "@mobily/ts-belt"
-import debtShed, { getDefaultDueDate } from "../debt"
+import { describe, expect } from "vitest"
 import billingPeriodShed from "../../../../lib/billing-period"
 import kopekShed from "../../../../lib/kopek"
+import { calculationResultItemsTotal } from "../calculation-result"
+import calculatorShed from "../calculator"
+import calculatorConfigShed from "../calculator-config"
+import debtShed, { getDefaultDueDate } from "../debt"
+import paymentShed, { Payment } from "../payment"
 
 const calculationDate = new Date("2024-04-19")
 const debtPeriod = billingPeriodShed.fromDate(new Date("2019-05-01"))
@@ -17,19 +19,64 @@ const naturalConfig = calculatorConfigShed.fromTheStateConstants(
     "natural",
     data
 )
+
 const initialDebt = debtShed.initDebt(
     debtPeriod,
     getDefaultDueDate(debtPeriod, data.daysToPay),
     debtAmount
 )
+const payments: Payment[] = [
+    {
+        id: paymentShed.numberToId(1),
+        date: new Date("2020-01-01"),
+        amount: kopekShed.fromRuble(100),
+    },
+]
+
 const calculator = pipe(
     calculatorShed.init(calculationDate, naturalConfig, "fifo"),
-    calculatorShed.addDebts([initialDebt])
+    calculatorShed.addDebts([initialDebt]),
+    calculatorShed.addPayments(payments)
 )
 
 describe("Калькулятор", () => {
+    const startDebtAmount = initialDebt.amount
     const result = calculatorShed.calculate(calculator)
-    it("Что-то вычисляет", () => {
-        expect(result[0].rows).toHaveLength(7)
+    const calculationResult = result[0]
+    const startDebtResultItems = calculationResult.rows.filter(
+        (row) => (row.debtAmount = startDebtAmount)
+    )
+    const one130DebtResultItems = calculationResult.rows.filter(
+        (row) => row.ratePart.denominator === 130
+    )
+
+    const expected = {
+        resultLength: 8,
+        penaltyAmount: kopekShed.fromRuble(835.05),
+        penaltyAmountOfStartDebt: kopekShed.fromRuble(103.04),
+        penaltyAmountOfOne130: calculationResultItemsTotal(
+            one130DebtResultItems
+        ),
+    }
+
+    it(`Количество строк расчета = ${expected.resultLength}`, () => {
+        expect(calculationResult.rows.length).toEqual(expected.resultLength)
+    })
+
+    it(`Сумма пеней = ${kopekShed.toRuble(expected.penaltyAmount)}`, () => {
+        expect(calculationResultItemsTotal(calculationResult.rows)).toBeCloseTo(
+            expected.penaltyAmount,
+            0
+        )
+    })
+
+    it(`Сумма пеней по долгу ${kopekShed.toRuble(
+        startDebtAmount
+    )} = ${kopekShed.toRuble(expected.penaltyAmountOfStartDebt)}`, () => {
+        expect(calculationResultItemsTotal(startDebtResultItems)).toBeCloseTo(
+            expected.penaltyAmountOfStartDebt,
+            0
+        )
     })
 })
+

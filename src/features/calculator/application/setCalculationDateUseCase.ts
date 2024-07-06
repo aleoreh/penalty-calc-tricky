1387bd0c68a5c48f8c39756f09672010744351e0
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-06 14:38:17 +0300

    Исправляет сценарий setCalculationDateUseCase
    - создаёт новую конфигурацию в зависимости от даты

--- src/features/calculator/application/setCalculationDateUseCase.ts
+++ src/features/calculator/application/setCalculationDateUseCase.ts
@@ -1,12 +1,25 @@
-import { CalculatorStoreRepo, SetCalculationDateUseCase } from "../domain"
+import { pipe } from "@mobily/ts-belt"
+import domain, {
+    CalculatorStoreRepo,
+    SetCalculationDateUseCase,
+} from "../domain"
 import calculatorShed from "../domain/calculator"
 
 export function createSetCalculationDateUseCase(
     calculatorStoreRepo: CalculatorStoreRepo
 ): SetCalculationDateUseCase {
     return async (calculationDate, calculator) => {
-        const newCalculator =
-            calculatorShed.setCalculationDate(calculationDate)(calculator)
+        const calculatorConfig = domain.fromTheStateConstants(
+            calculationDate,
+            calculator.userSettings.legalEntity,
+            calculator.config.theStateConstants
+        )
+
+        const newCalculator = pipe(
+            calculator,
+            calculatorShed.setConfig(calculatorConfig),
+            calculatorShed.setCalculationDate(calculationDate)
+        )
 
         await calculatorStoreRepo.setCalculator(newCalculator)
     }

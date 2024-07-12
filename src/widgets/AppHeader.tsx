a93d0293900558dd4700eac8a213dc1618e09a27
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-12 15:03:56 +0300

    Подключает модальный диалог в AppHeader

--- src/widgets/AppHeader.tsx
+++ src/widgets/AppHeader.tsx
@@ -1,16 +1,27 @@
-import { clsx } from "clsx"
-import styles from "./AppHeader.module.css"
 import { Icon } from "@iconify/react"
+import { clsx } from "clsx"
+import { Dialog } from "../components/Dialog"
+import styles from "./AppHeader.module.css"
+import { useState } from "react"
 
 export function AppHeader() {
+    const [isOpened, setIsOpened] = useState(false)
+
+    const handleClick = () => {
+        setIsOpened(true)
+    }
+
     return (
         <div className={styles.appHeader}>
             <div className={clsx("container", styles.container)}>
                 <h1>Калькулятор пеней ЖКХ</h1>
-                <button title="Настройки">
-                    <Icon icon="mdi-light:settings"/>
+                <button title="Настройки" onClick={handleClick}>
+                    <Icon icon="mdi-light:settings" />
                 </button>
             </div>
+            <Dialog isOpened={isOpened} close={() => setIsOpened(false)}>
+                <h3>Children</h3>
+            </Dialog>
         </div>
     )
 }

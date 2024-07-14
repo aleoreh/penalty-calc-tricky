49a0e8d0627ed752c6597a644fcaa2ac46a36733
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-14 13:08:24 +0300

    Улучшает useUserSettings

--- src/hooks/useUserSettings.ts
+++ src/hooks/useUserSettings.ts
@@ -3,6 +3,7 @@ import { useState } from "react"
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 import { useApplication } from "../contexts/applicationContextHook"
+import { getKeyRate } from "../features/calculator/domain/calculator"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
@@ -11,8 +12,32 @@ export function useUserSettings() {
     type Params = Parameters<typeof useCases.applyUserSettings>
     const [calculator, setCalculator] = useState(useCases.getCalculator())
 
+    const viewDistributionMethod =
+        calculator.userSettings.distributionMethod === "fifo"
+            ? "FIFO"
+            : "Первым - период оплаты"
+
+    const viewLegalEntity =
+        calculator.userSettings.legalEntity === "natural"
+            ? "Физические лица"
+            : "Юридические лица"
+
+    const keyRate = getKeyRate(calculator, calculator.calculationDate)
+
+    const viewKeyRate = new Intl.NumberFormat("ru-RU", {
+        style: "percent",
+    }).format(keyRate)
+
     return {
-        settings: calculator.userSettings,
+        settings: {
+            ...calculator.userSettings,
+            keyRate,
+        },
+        view: {
+            distributionMethod: viewDistributionMethod,
+            legalEntity: viewLegalEntity,
+            keyRate: viewKeyRate,
+        },
         setSettings: ([settings]: Params) => {
             useCases.applyUserSettings(settings)
             setCalculator(useCases.getCalculator())

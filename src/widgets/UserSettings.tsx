58dc8d8c76f91cf73db341dcc2562dfdf031077b
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-14 13:14:41 +0300

    Улучшает UserSettings.tsx

--- src/widgets/UserSettings.tsx
+++ src/widgets/UserSettings.tsx
@@ -7,29 +7,34 @@ import Typography from "@mui/material/Typography"
 
 import { useRegularText } from "../components/useRegularText"
 import { useUserSettings } from "../hooks/useUserSettings"
+import { useSectionTitle } from "../components/useSectionTitle"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 export function UserSettings() {
     const userSettingsInfo = useRegularText()
+    const userSettingsTitle = useSectionTitle()
     const { view } = useUserSettings()
 
     return (
-        <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-        >
-            <Typography {...userSettingsInfo}>
-                {`${[
-                    view.legalEntity,
-                    view.distributionMethod,
-                    view.keyRate,
-                ].join("; ")}`}
-            </Typography>
-            <IconButton>
-                <Edit />
-            </IconButton>
-        </Stack>
+        <>
+            <Typography {...userSettingsTitle}>Настройки расчёта</Typography>
+            <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+            >
+                <Typography {...userSettingsInfo}>
+                    {`${[
+                        view.legalEntity,
+                        view.distributionMethod,
+                        view.keyRate,
+                    ].join("; ")}`}
+                </Typography>
+                <IconButton>
+                    <Edit />
+                </IconButton>
+            </Stack>
+        </>
     )
 }

5811529b0c537218a795dfaa7c678c3fba212015
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-14 12:33:31 +0300

    Исправляет AppHeader

--- src/widgets/AppHeader.tsx
+++ src/widgets/AppHeader.tsx
@@ -1,8 +1,10 @@
 import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
 import { useAppTitle } from "../components/useAppTitle"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -10,17 +12,15 @@ import { useAppTitle } from "../components/useAppTitle"
 export function AppHeader() {
     const title = useAppTitle()
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Container>
-                    <Toolbar>
-                        <Typography {...title} sx={{ flexGrow: 1 }}>
-                            Калькулятор пеней ЖКХ
-                        </Typography>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-        </Box>
+        <AppBar position="static">
+            <Container>
+                <Toolbar>
+                    <Typography {...title} sx={{ flexGrow: 1 }}>
+                        Калькулятор пеней ЖКХ
+                    </Typography>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 

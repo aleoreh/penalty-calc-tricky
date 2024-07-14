ddf240335f1d5ddad55db66a62a1bceff65711b3
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-14 13:09:00 +0300

    Использует useUserSettings в UserSettings.tsx

--- src/widgets/UserSettings.tsx
+++ src/widgets/UserSettings.tsx
@@ -1,3 +1,5 @@
+import { Edit } from "@mui/icons-material"
+import IconButton from "@mui/material/IconButton"
 import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
 
@@ -10,13 +12,24 @@ import { useUserSettings } from "../hooks/useUserSettings"
 
 export function UserSettings() {
     const userSettingsInfo = useRegularText()
-    const { settings } = useUserSettings()
+    const { view } = useUserSettings()
 
     return (
-        <Stack direction="row">
+        <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+        >
             <Typography {...userSettingsInfo}>
-                {JSON.stringify(settings, undefined, 2)}
+                {`${[
+                    view.legalEntity,
+                    view.distributionMethod,
+                    view.keyRate,
+                ].join("; ")}`}
             </Typography>
+            <IconButton>
+                <Edit />
+            </IconButton>
         </Stack>
     )
 }

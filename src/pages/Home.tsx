ddace26f696a1fa8eeb170caf3a691d6b39a5625
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-14 12:39:03 +0300

    Добавляет UserSettings на страницу Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,19 @@
-import { useState } from "react"
-import { useApplication as useApplicationUseCases } from "../contexts/applicationContextHook"
-import domain from "../features/calculator/domain"
-import { billingPeriodFromDate } from "../lib/billing-period"
-import { kopekFromRuble } from "../lib/kopek"
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
 
-function useCalculator() {
-    const { useCases } = useApplicationUseCases()
-    const [calculator, setCalculator] = useState(useCases.getCalculator())
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
-    const dispatch = (callback: () => void) => {
-        callback()
-        setCalculator(useCases.getCalculator())
-    }
+import { UserSettings } from "../widgets/UserSettings"
 
-    return {
-        calculator,
-        dispatch,
-        ...useCases,
-    }
-}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 export function Home() {
-    const { calculator, dispatch, addCalculatorDebt: addDebt } = useCalculator()
-
-    const clickHandler = () => {
-        const debtPeriod = billingPeriodFromDate(new Date("2024-01-01"))
-        const dueDate = domain.getDefaultDueDate(debtPeriod, 10)
-
-        dispatch(() => addDebt(debtPeriod, dueDate, kopekFromRuble(1000)))
-    }
-
     return (
-        <div className="container">
-            <button>Нажми меня!</button>
-            <div onClick={clickHandler}>
-                <pre>{JSON.stringify(calculator, undefined, 4)}</pre>
-            </div>
-        </div>
+        <Box>
+            <Container>
+                <UserSettings />
+            </Container>
+        </Box>
     )
 }
 

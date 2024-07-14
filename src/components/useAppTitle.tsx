84ad21c43bd764ae6a4ee6120d6bb15b0087db97
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-14 12:07:31 +0300

    Добавляет useAppTitle

---
+++ src/components/useAppTitle.tsx
@@ -0,0 +1,8 @@
+import { TypographyProps } from "@mui/material";
+
+export function useAppTitle(): TypographyProps {
+    return {
+        component: "h1",
+        variant: "h6"
+    }
+}
--- src/widgets/AppHeader.tsx
+++ src/widgets/AppHeader.tsx
@@ -3,20 +3,18 @@ import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
+import { useAppTitle } from "../components/useAppTitle"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 export function AppHeader() {
+    const title = useAppTitle()
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Container>
                     <Toolbar>
-                        <Typography
-                            variant="h6"
-                            component="div"
-                            sx={{ flexGrow: 1 }}
-                        >
+                        <Typography {...title} sx={{ flexGrow: 1 }}>
                             Калькулятор пеней ЖКХ
                         </Typography>
                     </Toolbar>

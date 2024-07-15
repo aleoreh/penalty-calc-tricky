46e876076d5c22cdbb5b3af0b9067eeef57c08f9
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-15 17:07:04 +0300

    WIP: диалог добавления долга

--- src/widgets/DebtsList/DebtsList.tsx
+++ src/widgets/DebtsList/DebtsList.tsx
@@ -1,15 +1,24 @@
 import { ExpandMore } from "@mui/icons-material"
 import Accordion from "@mui/material/Accordion"
+import AccordionActions from "@mui/material/AccordionActions"
 import AccordionDetails from "@mui/material/AccordionDetails"
 import AccordionSummary from "@mui/material/AccordionSummary"
+import Button from "@mui/material/Button"
+import Stack from "@mui/material/Stack"
+import TextField from "@mui/material/TextField"
 import Typography from "@mui/material/Typography"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 import { billingPeriodFromDate } from "@/lib/billing-period"
 import kopekShed from "@/lib/kopek"
+import { ModalForm } from "../../components/ModalForm"
+import { useModalForm } from "../../components/useModalForm"
 import { useSectionTitle } from "../../components/useSectionTitle"
+import { useValidatedForm } from "../../components/useValidatedForm"
+import { useValidatedInput } from "../../components/useValidatedInput"
 import { useDebts } from "../../hooks/useDebts"
+import { validationDecoders } from "../../validation/validationDecoders"
 import { DebtItem } from "./DebtItem"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -25,16 +34,42 @@ export function DebtsList() {
         },
     ]
     const title = useSectionTitle()
+
+    const debtAmountInput = useValidatedInput(
+        undefined,
+        validationDecoders.decimal
+    )
+
+    const modalForm = useModalForm()
+    const validatedForm = useValidatedForm([debtAmountInput])
+
     return (
-        <Accordion>
-            <AccordionSummary expandIcon={<ExpandMore />}>
-                <Typography {...title}>Долги</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                {debts.map((x, i) => (
-                    <DebtItem key={i} debt={x} />
-                ))}
-            </AccordionDetails>
-        </Accordion>
+        <>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMore />}>
+                    <Typography {...title}>Долги</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {debts.map((x, i) => (
+                        <DebtItem key={i} debt={x} />
+                    ))}
+                </AccordionDetails>
+                <AccordionActions>
+                    <Button type="button" onClick={modalForm.open}>
+                        Добавить
+                    </Button>
+                    <Button>Добавить несколько</Button>
+                </AccordionActions>
+            </Accordion>
+            <ModalForm title="Добавить долг" {...modalForm} {...validatedForm}>
+                <Stack>
+                    <TextField
+                        {...debtAmountInput.input}
+                        label="Сумма"
+                        required
+                    />
+                </Stack>
+            </ModalForm>
+        </>
     )
 }

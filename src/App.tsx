c509ad389c4a25671d90e66c73f18b40762aabd8
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-15 14:37:56 +0300

    Устанавливает и подключает x-date-pickers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { Box, CssBaseline } from "@mui/material"
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import { ruRU } from "@mui/x-date-pickers/locales"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import "dayjs/locale/ru"
 import { useEffect, useState } from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
@@ -14,6 +18,35 @@ import { Home } from "./pages/Home"
 import { AppHeader } from "./widgets/AppHeader"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+const theme = createTheme(
+    {
+        palette: {
+            primary: { main: "#1976d2" },
+        },
+        components: {
+            MuiContainer: {
+                defaultProps: {
+                    maxWidth: "md",
+                },
+            },
+            MuiStack: {
+                defaultProps: {
+                    useFlexGap: true,
+                    gap: 2,
+                },
+            },
+            MuiAccordion: {
+                defaultProps: {
+                    variant: "outlined",
+                },
+            },
+        },
+    },
+    ruRU
+)
+
+theme.spacing(2)
 
 const router = createBrowserRouter([
     {
@@ -44,13 +77,20 @@ function App() {
     return (
         calculator && (
             <ApplicationProvider dependencies={createDependencies(calculator)}>
-                <>
-                    <CssBaseline />
-                    <AppHeader />
-                    <Box component="main">
-                        <RouterProvider router={router} />
-                    </Box>
-                </>
+                <ThemeProvider theme={theme}>
+                    <LocalizationProvider
+                        dateAdapter={AdapterDayjs}
+                        adapterLocale="ru"
+                    >
+                        <>
+                            <CssBaseline />
+                            <AppHeader />
+                            <Box component="main">
+                                <RouterProvider router={router} />
+                            </Box>
+                        </>
+                    </LocalizationProvider>
+                </ThemeProvider>
             </ApplicationProvider>
         )
     )

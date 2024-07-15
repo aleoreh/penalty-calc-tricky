0306a8c54e68b2f53d31a751f735e461e27fa9a2
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-15 14:57:17 +0300

    Выводит InputCalculationDate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
+import Stack from "@mui/material/Stack"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 import { CalculationConfig } from "../widgets/CalculationConfig"
+import { InputCalculationDate } from "../widgets/InputCalculationDate"
 import { UserSettings } from "../widgets/UserSettings"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -12,8 +14,11 @@ export function Home() {
     return (
         <Box>
             <Container>
-                <UserSettings />
-                <CalculationConfig />
+                <Stack>
+                    <UserSettings />
+                    <CalculationConfig />
+                    <InputCalculationDate />
+                </Stack>
             </Container>
         </Box>
     )

d76295b10ad94c8d20c74b8e44094c26d662b151
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-15 14:07:26 +0300

    Завершает функциональность UserSettings.tsx

--- src/widgets/UserSettings.tsx
+++ src/widgets/UserSettings.tsx
@@ -1,12 +1,16 @@
 import { Edit } from "@mui/icons-material"
+import FormControl from "@mui/material/FormControl"
 import IconButton from "@mui/material/IconButton"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+import Select, { SelectChangeEvent } from "@mui/material/Select"
 import Stack from "@mui/material/Stack"
+import TextField from "@mui/material/TextField"
 import Typography from "@mui/material/Typography"
+import { useState } from "react"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
-import TextField from "@mui/material/TextField"
-import { string } from "decoders"
 import { ModalForm } from "../components/ModalForm"
 import { useModalForm } from "../components/useModalForm"
 import { useRegularText } from "../components/useRegularText"
@@ -14,18 +18,60 @@ import { useSectionTitle } from "../components/useSectionTitle"
 import { useValidatedForm } from "../components/useValidatedForm"
 import { useValidatedInput } from "../components/useValidatedInput"
 import { useUserSettings } from "../hooks/useUserSettings"
+import { validationDecoders } from "../validation/validationDecoders"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 export function UserSettings() {
     const userSettingsInfo = useRegularText()
     const userSettingsTitle = useSectionTitle()
-    const { view } = useUserSettings()
-    const field = useValidatedInput(
-        string.refine((x) => x === "hello", "Здесь должно быть слово hello")
+    const { view, settings, isLegalEntity, isDistributionMethod, setSettings } =
+        useUserSettings()
+
+    const [legalEntity, setLegalEntity] = useState(settings.legalEntity)
+    const [distributionMethod, setDistributionMethod] = useState(
+        settings.distributionMethod
     )
-    const validatedForm = useValidatedForm([field])
+
+    const legalEntityInput = useValidatedInput(
+        settings.legalEntity,
+        validationDecoders.nonEmptyString
+    )
+    const distributionMethodInput = useValidatedInput(
+        settings.distributionMethod,
+        validationDecoders.nonEmptyString
+    )
+    const keyRateInput = useValidatedInput(
+        settings.calculationKeyRate === undefined
+            ? undefined
+            : settings.calculationKeyRate * 100,
+        validationDecoders.decimal.transform((x) => x / 100)
+    )
+    const validatedForm = useValidatedForm([
+        legalEntityInput,
+        distributionMethodInput,
+        keyRateInput,
+    ])
     const modalForm = useModalForm()
+
+    const handleLegalEntityChange = (event: SelectChangeEvent) => {
+        isLegalEntity(event.target.value) && setLegalEntity(event.target.value)
+    }
+
+    const handleDistributionMethodChange = (event: SelectChangeEvent) => {
+        isDistributionMethod(event.target.value) &&
+            setDistributionMethod(event.target.value)
+    }
+
+    const submit = () => {
+        setSettings([
+            {
+                legalEntity,
+                distributionMethod,
+                calculationKeyRate: keyRateInput.validatedValue,
+            },
+        ])
+    }
 
     return (
         <>
@@ -37,8 +83,8 @@ export function UserSettings() {
             >
                 <Typography {...userSettingsInfo}>
                     {`${[
-                        view.legalEntity,
-                        view.distributionMethod,
+                        view.legalEntity(settings.legalEntity),
+                        view.distributionMethod(settings.distributionMethod),
                         view.keyRate,
                     ].join("; ")}`}
                 </Typography>
@@ -47,16 +93,46 @@ export function UserSettings() {
                 </IconButton>
             </Stack>
             <ModalForm
-                title="Диалог"
+                title="Настройки"
                 {...modalForm}
                 {...validatedForm}
-                submit={() => {
-                    console.log(field.validatedValue)
-                }}
-                submitAndContinue={() => {}}
+                submit={submit}
             >
-                <TextField {...field.input} title="Значение" required/>
-                <Typography>{field.validatedValue}</Typography>
+                <FormControl>
+                    <InputLabel id="legal-entity-input-label">
+                        Порядок расчёта
+                    </InputLabel>
+                    <Select
+                        labelId="legal-entity-input-label"
+                        id="legal-entity-input"
+                        value={legalEntity}
+                        label="Порядок расчёта"
+                        onChange={handleLegalEntityChange}
+                    >
+                        <MenuItem value="natural">Физические лица</MenuItem>
+                        <MenuItem value="artificial">Юридические лица</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl>
+                    <InputLabel id="distribution-method-input-label">
+                        Метод распределения
+                    </InputLabel>
+                    <Select
+                        labelId="distribution-method-input-label"
+                        id="distribution-method-input"
+                        value={distributionMethod}
+                        label="Метод распределения"
+                        onChange={handleDistributionMethodChange}
+                    >
+                        <MenuItem value="fifo">
+                            {view.distributionMethod("fifo")}
+                        </MenuItem>
+                        <MenuItem value="byPaymentPeriod">
+                            {view.distributionMethod("byPaymentPeriod")}
+                        </MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField {...keyRateInput.input} title="Значение" required />
             </ModalForm>
         </>
     )

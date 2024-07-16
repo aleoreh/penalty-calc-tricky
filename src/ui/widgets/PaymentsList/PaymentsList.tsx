5ba0493520004490a8ed8c48951f9eb74a7c8203
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-16 14:21:29 +0300

    Заполняет PaymentsList.tsx

--- src/ui/widgets/PaymentsList/PaymentsList.tsx
+++ src/ui/widgets/PaymentsList/PaymentsList.tsx
@@ -1,3 +1,115 @@
+import { ExpandMore } from "@mui/icons-material"
+import Accordion from "@mui/material/Accordion"
+import AccordionActions from "@mui/material/AccordionActions"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import Button from "@mui/material/Button"
+import Stack from "@mui/material/Stack"
+import TextField from "@mui/material/TextField"
+import Typography from "@mui/material/Typography"
+import { DatePicker } from "@mui/x-date-pickers/DatePicker"
+import { Dayjs } from "dayjs"
+import { useState } from "react"
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+import { billingPeriodFromDate } from "@/lib/billing-period"
+import { ModalForm } from "@/ui/components/ModalForm"
+import { useModalForm } from "@/ui/components/useModalForm"
+import { useSectionTitle } from "@/ui/components/useSectionTitle"
+import { useValidatedForm } from "@/ui/components/useValidatedForm"
+import { useValidatedInput } from "@/ui/components/useValidatedInput"
+import { usePayments } from "@/ui/hooks/usePayments"
+import { validationDecoders } from "@/ui/validation/validationDecoders"
+import { kopekFromRuble } from "../../../lib/kopek"
+import { PaymentItem } from "./PaymentItem"
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
 export function PaymentsList() {
-    return <h2>PaymentsList</h2>
+    const [inputPaymentDate, setInputPaymentDate] = useState<Dayjs | null>(null)
+    const [inputPaymentPeriod, setInputPaymentPeriod] = useState<Dayjs | null>(
+        null
+    )
+
+    const { payments, addPayment } = usePayments()
+
+    const sectionTitle = useSectionTitle()
+
+    const modalForm = useModalForm()
+
+    const paymentAmountInput = useValidatedInput("", validationDecoders.decimal)
+    const addPaymentValidatedForm = useValidatedForm([paymentAmountInput])
+
+    const submitAddPayment = () => {
+        if (
+            inputPaymentDate === null ||
+            paymentAmountInput.validatedValue === undefined
+        ) {
+            return
+        }
+
+        addPayment(
+            inputPaymentDate.toDate(),
+            kopekFromRuble(paymentAmountInput.validatedValue),
+            inputPaymentPeriod
+                ? billingPeriodFromDate(inputPaymentPeriod.toDate())
+                : undefined
+        )
+
+        setInputPaymentDate(null)
+        setInputPaymentPeriod(null)
+    }
+
+    const handleInputPaymentPeriodChange = () => {}
+
+    return (
+        <>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMore />}>
+                    <Typography {...sectionTitle}>Оплата</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Stack>
+                        {payments.map((payment) => (
+                            <PaymentItem payment={payment} />
+                        ))}
+                    </Stack>
+                </AccordionDetails>
+                <AccordionActions>
+                    <Button type="button" onClick={modalForm.open}>
+                        Добавить
+                    </Button>
+                    <Button>Добавить несколько</Button>
+                </AccordionActions>
+            </Accordion>
+            <ModalForm
+                title="Добавить оплату"
+                {...modalForm}
+                {...addPaymentValidatedForm}
+                submit={submitAddPayment}
+            >
+                <Stack>
+                    <DatePicker
+                        label={"Дата платежа"}
+                        value={inputPaymentDate}
+                        onChange={setInputPaymentDate}
+                    />
+                    <DatePicker
+                        label={"Период"}
+                        value={inputPaymentPeriod}
+                        onChange={handleInputPaymentPeriodChange}
+                        views={["year", "month"]}
+                        openTo="year"
+                    />
+                    <TextField
+                        {...paymentAmountInput.input}
+                        label="Сумма"
+                        required
+                    />
+                </Stack>
+            </ModalForm>
+        </>
+    )
 }
+

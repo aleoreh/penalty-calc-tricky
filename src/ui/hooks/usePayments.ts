6af7e267483b6915f98df003ef7cbb624b9a89d6
Aleksey Orekhov <aleoreh@gmail.com>
2024-07-16 16:42:29 +0300

    Добавляет удаление оплаты в usePayments

--- src/ui/hooks/usePayments.ts
+++ src/ui/hooks/usePayments.ts
@@ -3,14 +3,26 @@ import { useApplication } from "./useApplication"
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 export function usePayments() {
-    const { dispatch, calculator, addCalculatorPayment } = useApplication()
+    const {
+        dispatch,
+        calculator,
+        addCalculatorPayment,
+        deleteCalculatorPayment,
+    } = useApplication()
 
     const addPayment = (...params: Parameters<typeof addCalculatorPayment>) => {
         dispatch(() => addCalculatorPayment(...params))
     }
 
+    const deletePayment = (
+        ...params: Parameters<typeof deleteCalculatorPayment>
+    ) => {
+        dispatch(() => deleteCalculatorPayment(...params))
+    }
+
     return {
         payments: calculator.payments,
         addPayment,
+        deletePayment,
     }
 }
